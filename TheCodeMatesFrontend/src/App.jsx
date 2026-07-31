3813aa874162cbc414a78a1911a69fed72a8618d
Kaustubh-Dalvi1001 <kaustubhdalvi1001@gmail.com>
2026-07-31 13:51:17 +0530

    added tailwind, daisy ui, navbar and app logo.

--- TheCodeMatesFrontend/src/App.jsx
+++ TheCodeMatesFrontend/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div> React Project </div>;
+  return (
+    <div>
+      <Header />
+      <h1>hello</h1>
+    </div>
+  );
 }
 
 export default App;
